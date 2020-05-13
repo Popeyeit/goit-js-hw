935c1b42802aa94e4ca469cca9b372217f24c7ce
PopeyeIT <IgorSerov28@gmail.com>
2020-05-13 13:28:47 +0300

    solve task-6 in different versions

--- hw-01/js/task-06.js
+++ hw-01/js/task-06.js
@@ -2,44 +2,59 @@
 let input = 0;
 let total = 0;
 
-
-
-for (let i; i => 0;) {
-
-    input = prompt('Введите число');
-    const counterInput = Number(input)
-    i = +counterInput
-    if (!isNaN(counterInput)) {
-        total += i
-    }
-
-    if (isNaN(counterInput)) {
-        alert('Было введено не число, попробуйте еще раз');
-        continue;
-    }
-    console.log(typeof counterInput);
-    if (counterInput === 0) {
-        alert(`Общая сумма чисел равна ${total}`);
-        break;
-    }
-}
-console.log(total);
-
-// do {
+// for (let i = 0; i >= 0;) {
 //     input = prompt('Введите число');
-//     let counterInput = Number(input)
-//     console.log(typeof input);
-//     let i;
-//     i = +counterInput;
-//     total += i;
+//     const counterInput = Number(input);
+//     console.log(counterInput)
+//     if (!isNaN(counterInput)) {
+//         total += counterInput;
+//     }
+
 //     if (isNaN(counterInput)) {
 //         alert('Было введено не число, попробуйте еще раз');
 //         continue;
 //     }
-
+//     console.log(typeof counterInput);
 //     if (counterInput === 0) {
 //         alert(`Общая сумма чисел равна ${total}`);
 //         break;
 //     }
+// }
 
-// } while (input > 0);
+
+
+// console.log(total);
+
+// do {
+//     input = prompt('Введите число');
+//     input = Number(input);
+//     console.log(typeof input);
+//     if (isNaN(input)) {
+//         alert('Было введено не число, попробуйте еще раз');
+//         continue;
+//     }
+//     total += input;
+
+//     if (input === 0) {
+//         alert(`Общая сумма чисел равна ${total}`);
+//         break;
+//     }
+
+// } while (input !== 0);
+
+
+// for (let i = 0; i >= 0;) {
+//     input = prompt('Введите число');
+//     console.log(input)
+//     if (isNaN(input)) {
+//         alert('Было введено не число, попробуйте еще раз');
+//         continue;
+//     }
+//     total += Number(input);
+//     console.log(typeof input);
+//     if (input === null) {
+//         alert(`Общая сумма чисел равна ${total}`);
+//         break;
+//     }
+// }
+// console.log(total)
